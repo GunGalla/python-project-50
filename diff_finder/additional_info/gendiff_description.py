b7cdd9a863a5083a0e0063ffbe55735a197d1a84
Iaroslav <GunGalla@gmail.com>
2023-01-31 14:12:01 +0200

    Add -f option to gendiff description

--- diff_finder/additional_info/gendiff_description.py
+++ diff_finder/additional_info/gendiff_description.py
@@ -9,4 +9,5 @@ def description():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.parse_args()
