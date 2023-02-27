c2055803e66334f692f3f8d0f45552b2640757ba
Iaroslav <GunGalla@gmail.com>
2023-02-27 13:52:07 +0200

    Add files parsing for simple jsons

--- diff_finder/gendiff.py
+++ diff_finder/gendiff.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 """Gendiff run script."""
+import json
+
 from diff_finder.additional_info.gendiff_description import description
 
 
-def main():
-    """Starts gendiff."""
-    description()
+def generate_diff(file_path1, file_path2):
+    data1 = json.load(open(file_path1))
+    data2 = json.load(open(file_path2))
+
+    diff = {}
+
+    for key in sorted(set(data1.keys()) | set(data2.keys())):
+        if key not in data1:
+            diff[f'  + {key}'] = str(data2[key]).lower()
+        elif key not in data2:
+            diff[f'  - {key}'] = str(data1[key]).lower()
+        elif data1[key] != data2[key]:
+            diff[f'  - {key}'] = str(data1[key]).lower()
+            diff[f'  + {key}'] = str(data2[key]).lower()
+        else:
+            diff[f'    {key}'] = str(data1[key]).lower()
+    result = '{\n'
+    result += '\n'.join([f'{key}: {value}' for key, value in diff.items()])
+    result += '\n}'
+    return result
 
 
 if __name__ == '__main__':
-    main()
+    generate_diff('files/file1.json', 'files/file2.json')
+    description()
