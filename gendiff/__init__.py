e4e44caf5dc2a06eadb6d37cbf6e5cc54b6f7fda
Iaroslav <GunGalla@gmail.com>
2023-03-01 14:08:16 +0200

    Mod __init__.py for gendiff package

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,1 +1,6 @@
 """__init__.py for gendiff directory."""
+from gendiff.generate_diff import generate_diff
+from gendiff.gendiff_argparse import parse_args
+
+
+__all__ = ('generate_diff', 'parse_args',)
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,6 +2,7 @@
 from gendiff.parse_files import parse_file
 from gendiff.define_diff import define_difference
 from gendiff.formatter.format import make_format
+import typing
 
 
 def generate_diff(file_path1, file_path2, format='stylish'):
@@ -11,3 +12,6 @@ def generate_diff(file_path1, file_path2, format='stylish'):
     diff = define_difference(data1, data2)
 
     return make_format(diff, format)
+
+if __name__ == '__main__':
+    print(isinstance(generate_diff, typing.Callable))
