4e1f616cc97dadb14ca65fb81a28b9521aa7f715
Iaroslav <GunGalla@gmail.com>
2023-03-01 20:06:06 +0200

    Refactoring stylish.py

--- gendiff/formatter/stylish.py
+++ gendiff/formatter/stylish.py
@@ -23,7 +23,8 @@ def make_stylish(items, depth=0):
 
         elif item['action'] == 'parent':
             lines.append(
-                f"{indent}    {item['key']}: {make_stylish(item['child'], depth+1)}"
+                f"{indent}    {item['key']}:"
+                f" {make_stylish(item['child'], depth+1)}"
             )
 
     result = itertools.chain("{", lines, [indent + "}"])
@@ -38,27 +39,18 @@ def build_line(key, value, sign, depth):
     lines = []
 
     if isinstance(value, dict):
-        lines.append(
-            f'{indent}  {sign} {key}: {restruct_dict(value, depth+1)}'
-        )
+        dict_lines = []
+        for k, v in sorted(value.items()):
+            dict_lines.append(build_line(k, v, ' ', depth + 1))
 
+        result = itertools.chain("{", dict_lines, [indent + "    }"])
+
+        line = '\n'.join(result)
+        lines.append(f'{indent}  {sign} {key}: {line}')
     else:
         lines.append(f'{indent}  {sign} {key}: {format_value(value)}')
 
     return '\n'.join(lines)
-
-
-def restruct_dict(items, depth):
-    """Restruct dict to string."""
-    lines = []
-    indent = '    ' * depth
-
-    for key, value in sorted(items.items()):
-        lines.append(build_line(key, value, ' ', depth))
-
-    result = itertools.chain("{", lines, [indent + "}"])
-
-    return '\n'.join(result)
 
 
 def format_value(value):
