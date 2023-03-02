b32040668cce7475c9b3e5cc0d46aa43592706ea
Iaroslav <GunGalla@gmail.com>
2023-03-02 10:41:57 +0200

    Some fixes

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,8 +6,8 @@ from gendiff.formatter.format import make_format
 
 def generate_diff(file_path1, file_path2, format='stylish'):
     """Generates difference between two files"""
-    data1 = parse_file(open_file(file_path1), file_path1)
-    data2 = parse_file(open_file(file_path2), file_path2)
+    data1 = open_file(file_path1)
+    data2 = open_file(file_path2)
     diff = define_difference(data1, data2)
     result = make_format(diff, format)
     return result
--- gendiff/parse_files.py
+++ gendiff/parse_files.py
@@ -6,18 +6,13 @@ import yaml
 def open_file(filepath):
     """Check file format and load it."""
     with open(filepath, 'r') as f:
-        if filepath.endswith('.json'):
-            file = json.load(f)
-            return file
-        else:
-            file = f.read()
-            return file
+        return parse_file(f, filepath)
 
 
 def parse_file(data, filepath):
     """Returns parsed files"""
     if filepath.endswith('.json'):
-        return data
+        return json.load(data)
     elif filepath.endswith('.yaml') or filepath.endswith('.yml'):
         return yaml.safe_load(data)
     else:
